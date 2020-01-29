8b54301d4c1aced2ecf397ed59c49a6883dd0beb
EuCisneros <eucisnerosyt@gmail.com>
2020-01-29 14:19:02 -0600

    operaciones b

--- miprimerproyecto/Form1.Designer.cs
+++ miprimerproyecto/Form1.Designer.cs
@@ -34,13 +34,19 @@
             this.lblnum1 = new System.Windows.Forms.Label();
             this.lblnum2 = new System.Windows.Forms.Label();
             this.lblrespuesta = new System.Windows.Forms.Label();
+            this.grboperaciones = new System.Windows.Forms.GroupBox();
+            this.optsuma = new System.Windows.Forms.RadioButton();
+            this.optresta = new System.Windows.Forms.RadioButton();
+            this.optmulti = new System.Windows.Forms.RadioButton();
+            this.optdivi = new System.Windows.Forms.RadioButton();
+            this.grboperaciones.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnprocesar
             // 
             this.btnprocesar.BackColor = System.Drawing.Color.Bisque;
             this.btnprocesar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnprocesar.Location = new System.Drawing.Point(103, 214);
+            this.btnprocesar.Location = new System.Drawing.Point(146, 133);
             this.btnprocesar.Name = "btnprocesar";
             this.btnprocesar.Size = new System.Drawing.Size(75, 36);
             this.btnprocesar.TabIndex = 0;
@@ -50,14 +56,14 @@
             // 
             // txtnum1
             // 
-            this.txtnum1.Location = new System.Drawing.Point(72, 62);
+            this.txtnum1.Location = new System.Drawing.Point(57, 18);
             this.txtnum1.Name = "txtnum1";
             this.txtnum1.Size = new System.Drawing.Size(54, 20);
             this.txtnum1.TabIndex = 1;
             // 
             // txtnum2
             // 
-            this.txtnum2.Location = new System.Drawing.Point(210, 62);
+            this.txtnum2.Location = new System.Drawing.Point(167, 18);
             this.txtnum2.Name = "txtnum2";
             this.txtnum2.Size = new System.Drawing.Size(54, 20);
             this.txtnum2.TabIndex = 2;
@@ -66,7 +72,7 @@
             // 
             this.lblnum1.AutoSize = true;
             this.lblnum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblnum1.Location = new System.Drawing.Point(23, 65);
+            this.lblnum1.Location = new System.Drawing.Point(8, 21);
             this.lblnum1.Name = "lblnum1";
             this.lblnum1.Size = new System.Drawing.Size(47, 13);
             this.lblnum1.TabIndex = 3;
@@ -76,7 +82,7 @@
             // 
             this.lblnum2.AutoSize = true;
             this.lblnum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblnum2.Location = new System.Drawing.Point(161, 65);
+            this.lblnum2.Location = new System.Drawing.Point(118, 21);
             this.lblnum2.Name = "lblnum2";
             this.lblnum2.Size = new System.Drawing.Size(47, 13);
             this.lblnum2.TabIndex = 4;
@@ -86,18 +92,74 @@
             // 
             this.lblrespuesta.AutoSize = true;
             this.lblrespuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblrespuesta.Location = new System.Drawing.Point(100, 131);
+            this.lblrespuesta.Location = new System.Drawing.Point(70, 61);
             this.lblrespuesta.Name = "lblrespuesta";
             this.lblrespuesta.Size = new System.Drawing.Size(82, 13);
             this.lblrespuesta.TabIndex = 5;
             this.lblrespuesta.Text = "Respuesta: ?";
+            // 
+            // grboperaciones
+            // 
+            this.grboperaciones.Controls.Add(this.optdivi);
+            this.grboperaciones.Controls.Add(this.optmulti);
+            this.grboperaciones.Controls.Add(this.optresta);
+            this.grboperaciones.Controls.Add(this.optsuma);
+            this.grboperaciones.Location = new System.Drawing.Point(12, 105);
+            this.grboperaciones.Name = "grboperaciones";
+            this.grboperaciones.Size = new System.Drawing.Size(108, 100);
+            this.grboperaciones.TabIndex = 6;
+            this.grboperaciones.TabStop = false;
+            this.grboperaciones.Text = "Operaciones";
+            // 
+            // optsuma
+            // 
+            this.optsuma.AutoSize = true;
+            this.optsuma.Checked = true;
+            this.optsuma.Location = new System.Drawing.Point(7, 17);
+            this.optsuma.Name = "optsuma";
+            this.optsuma.Size = new System.Drawing.Size(52, 17);
+            this.optsuma.TabIndex = 0;
+            this.optsuma.TabStop = true;
+            this.optsuma.Text = "Suma";
+            this.optsuma.UseVisualStyleBackColor = true;
+            // 
+            // optresta
+            // 
+            this.optresta.AutoSize = true;
+            this.optresta.Location = new System.Drawing.Point(7, 38);
+            this.optresta.Name = "optresta";
+            this.optresta.Size = new System.Drawing.Size(53, 17);
+            this.optresta.TabIndex = 1;
+            this.optresta.Text = "Resta";
+            this.optresta.UseVisualStyleBackColor = true;
+            // 
+            // optmulti
+            // 
+            this.optmulti.AutoSize = true;
+            this.optmulti.Location = new System.Drawing.Point(7, 59);
+            this.optmulti.Name = "optmulti";
+            this.optmulti.Size = new System.Drawing.Size(89, 17);
+            this.optmulti.TabIndex = 2;
+            this.optmulti.Text = "Multiplicación";
+            this.optmulti.UseVisualStyleBackColor = true;
+            // 
+            // optdivi
+            // 
+            this.optdivi.AutoSize = true;
+            this.optdivi.Location = new System.Drawing.Point(7, 79);
+            this.optdivi.Name = "optdivi";
+            this.optdivi.Size = new System.Drawing.Size(63, 17);
+            this.optdivi.TabIndex = 3;
+            this.optdivi.Text = "Divición";
+            this.optdivi.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.ClientSize = new System.Drawing.Size(287, 273);
+            this.ClientSize = new System.Drawing.Size(263, 229);
+            this.Controls.Add(this.grboperaciones);
             this.Controls.Add(this.lblrespuesta);
             this.Controls.Add(this.lblnum2);
             this.Controls.Add(this.lblnum1);
@@ -106,6 +168,8 @@
             this.Controls.Add(this.btnprocesar);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.grboperaciones.ResumeLayout(false);
+            this.grboperaciones.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,6 +183,11 @@
         private System.Windows.Forms.Label lblnum1;
         private System.Windows.Forms.Label lblnum2;
         private System.Windows.Forms.Label lblrespuesta;
+        private System.Windows.Forms.GroupBox grboperaciones;
+        private System.Windows.Forms.RadioButton optdivi;
+        private System.Windows.Forms.RadioButton optmulti;
+        private System.Windows.Forms.RadioButton optresta;
+        private System.Windows.Forms.RadioButton optsuma;
     }
 }
 
--- miprimerproyecto/Form1.cs
+++ miprimerproyecto/Form1.cs
@@ -21,6 +21,16 @@ namespace miprimerproyecto
         {
             sbyte num1 = sbyte.Parse(txtnum1.Text);
             sbyte num2 = sbyte.Parse(txtnum2.Text);
+            int resp = 0; 
+            if( optsuma.Checked) {
+                resp = num1 + num2
+            }
+
+            if (optresta.Checked)
+            {
+                resp = num1 - num2
+            }
+
             lblrespuesta.Text = "Respuesta: " + (num1 + num2);
         }
     }
